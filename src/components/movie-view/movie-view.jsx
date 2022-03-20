9ede3aa9fda78b8e97c9fb0756b1c7f99a740a41
HienTrancong <89924593+HienTrancong@users.noreply.github.com>
2022-03-20 12:44:50 +0100

    Implement react-bootstrap to Views
    - Registration
    - Login
    - Main
    - Movie Card

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,38 +1,66 @@
 import React from 'react';
 
-export class MovieView extends React.Component {
+import { CardGroup, Card, Button, Container, CardImg, ListGroup, ListGroupItem, Row, Col } from 'react-bootstrap';
 
+export class MovieView extends React.Component {
   render() {
     const { movie, onBackClick } = this.props;
-
     return (
-      <div className="movie-view">
-        <div className="movie-poster">
-          <img src={movie.ImagePath} />
-        </div>
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-genre">
-          <span className="label">Genre: </span>
-          <span className="value">{movie.Genre.Name}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
-        </div>
-        <div className="movie-director">
-          <span className="label">Director: </span>
-          <span className="value">{movie.Director.Name}</span>
-        </div>
-        <button onClick={() => {onBackClick(null); }}>Back</button>
-      </div>
+      <Container>
+        <Row>
+          <Col>
+            <CardGroup>
+              <Card>
+                <CardImg className="movie-view movie-poster" src={movie.ImagePath} />
+                <Card.Body>
+                  <Card.Title className="movie-title value">{movie.Title}</Card.Title>
+                  <Card.Text className="movie-description value">{movie.Description}</Card.Text>
+                </Card.Body>
+                <Card.Body>
+                  <ListGroup>
+                    <ListGroupItem className="movie-genre">{movie.Genre.Name}</ListGroupItem>
+                    <ListGroupItem className="movie-director">{movie.Director.Name}</ListGroupItem>
+                  </ListGroup>
+                  <Button variant="Primary" onClick={() => { onBackClick(null); }}>
+                    Back
+                  </Button>
+                </Card.Body>
+              </Card>
+            </CardGroup>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
-
 /* NOTE
+
+    //   <div className="movie-view">
+    //     <div className="movie-poster">
+    //       <img src={movie.ImagePath} />
+    //     </div>
+    //     <div className="movie-title">
+    //       <span className="label">Title: </span>
+    //       <span className="value">{movie.Title}</span>
+    //     </div>
+    //     <div className="movie-genre">
+    //       <span className="label">Genre: </span>
+    //       <span className="value">{movie.Genre.Name}</span>
+    //     </div>
+    //     <div className="movie-description">
+    //       <span className="label">Description: </span>
+    //       <span className="value">{movie.Description}</span>
+    //     </div>
+    //     <div className="movie-director">
+    //       <span className="label">Director: </span>
+    //       <span className="value">{movie.Director.Name}</span>
+    //     </div>
+    //     <button onClick={() => {onBackClick(null); }}>Back</button>
+    //   </div>
+    // );
+  }
+
+
 keypressCallback(event) {
   console.log(event.key);
 }
--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,10 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+//Import PropTypes to control type of props passed
+import PropTypes from "prop-types";
+//Import registrationView css file
+import "./registration-view.scss";
+
+import { Form, Button, Card, CardGroup, Container, Col, Row } from "react-bootstrap";
 
 export function RegistrationView(props) {
-  const [ username, setUsername ] = useState('');
-  const [ password, setPassword ] = useState('');
-  const [ email, setEmail ] = useState('');
-  const [ birthday, setBirthday ] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthday, setBirthday] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -12,13 +18,64 @@ export function RegistrationView(props) {
     props.onRegistration(username);
   };
 
-  return(
-    <form>
-      <label>Username: <input type="text" value={username} onChange= { e => setUsername(e.target.value) } /> </label>
-      <label>Password: <input type="password" value={password} onChange= { e => setPassword(e.target.value) } /></label>
-      <label>Email: <input type="email" value={email} onChange= { e => setEmail(e.target.value)} /></label>
-      <label>Birthday: <input type="date" value={birthday} onChange= { e => setBirthday(e.target.value)} /></label>
-      <button type="submit" onClick={handleSubmit} >Register</button>
-    </form>
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <CardGroup>
+            <Card>
+              <Card.Body>
+                <Card.Title>Please register</Card.Title>
+                <Form>
+                  <Form.Group>
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                      type="text"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                      placeholder="Enter a username"
+                      required
+                    />
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                      type="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      placeholder="Your password must be 8 or more characters"
+                      minLength="8"
+                      required
+                    />
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                      type="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      placeholder="Enter your email address"
+                      required
+                    />
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label>Birthday</Form.Label>
+                    <Form.Control
+                      type="date"
+                      value={birthday}
+                      onChange={(e) => setBirthday(e.target.value)}
+                      placeholder="Enter your day of birth"
+                    />
+                  </Form.Group>
+                  <Button variant="primary" type="submit" onClick={handleSubmit}>
+                    Register
+                  </Button>
+                </Form>
+              </Card.Body>
+            </Card>
+          </CardGroup>
+        </Col>
+      </Row>
+    </Container>
   );
 }
