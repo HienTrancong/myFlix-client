e4f5161241fe2d4a4eb6b91434c0ecc02ae2d3ef
HienTrancong <89924593+HienTrancong@users.noreply.github.com>
2022-05-27 17:10:50 +0200

    create store

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,36 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Container from "react-bootstrap/Container";
-import { MainView } from "./components/main-view/main-view";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Container from 'react-bootstrap/Container';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+import moviesApp from './reducers/reducers';
+import { MainView } from './components/main-view/main-view';
 
-import "./index.scss";
+import './index.scss';
+
+// Declare store with reducer as argument
+const myFlixStore = createStore(moviesApp, devToolsEnhancer());
 
 //Create class component 'MyFlixApplication' which is main component
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <Container>
-        <MainView />
-      </Container>
+      <Provider store={myFlixStore}>
+        <Container>
+          <MainView />
+        </Container>
+      </Provider >
     );
   }
 }
 
 //DOM finds the root of the app and assign to constant container
-const container = document.getElementsByClassName("app-container")[0];
+const container = document.getElementsByClassName('app-container')[0];
 
 //Tells React to render app in the root DOM element, display MyFlicApplication component in the 'root' element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
 
-/* NOTE
+/* 
 
 */
